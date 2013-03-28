9ebae35d8c8b623e4a00239d7056385a00ef8620
root <root@guest-6.(none)>
2013-03-28 10:02:01 +0100

    fix

--- irgatimolurmusun/wsgi.py
+++ irgatimolurmusun/wsgi.py
@@ -13,8 +13,9 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os,sys
 
+sys.path.append("/home/ercanbasa/siteler/IrgatimOlurmusun")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "irgatimolurmusun.settings")
 
 # This application object is used by any WSGI server configured to use this
