dcf6004b3b30c382ecaf4d37b5c2158ae177533c
ErhanBasa <erhanbasa@Erhan-MacBook-Pro.local>
2013-04-02 07:55:22 +0300

    fixes style

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,19 @@
 
 $(document).ready(function(){
 
+	var wChair = $('#Container .workerChair');
+	var wCItem = $('#Container .workerChair span');
+	var bChair = $('#Container .bossChair');
+	var bCItem = $('#Container .bossChair span');
+	var textContainer = $('#Container .textContainer');
+
 	var width = $(window).width();
 	if (width < 1000) {
-	$('#Container .workerChair').animate({'right':'8%'},500, function(){	
-		$('#Container .workerChair span').animate({'top':'80px','opacity':'1'},800, function(){
-			$('#Container .bossChair').animate({'left':'8%'},1200,function(){
-				$('#Container .bossChair span').animate({'top':'73px','opacity':'1'},1000, function(){
-					$('#Container .textContainer').animate({'bottom':'350px'},800);
+	wChair.animate({'right':'8%'},500, function(){	
+		wCItem.animate({'top':'80px','opacity':'1'},800, function(){
+			bChair.animate({'left':'8%'},1200,function(){
+				bCItem.animate({'top':'73px','opacity':'1'},1000, function(){
+					textContainer.animate({'bottom':'350px'},800);
 				});
 			});
 		});
@@ -15,11 +21,11 @@ $(document).ready(function(){
 		
 	}
 	else {
-	$('#Container .workerChair').animate({'right':'30%'},500, function(){	
-		$('#Container .workerChair span').animate({'top':'80px','opacity':'1'},800, function(){
-			$('#Container .bossChair').animate({'left':'30%'},1200,function(){
-				$('#Container .bossChair span').animate({'top':'73px','opacity':'1'},1000, function(){
-					$('#Container .textContainer').animate({'bottom':'350px'},800);
+	wChair.animate({'right':'30%'},500, function(){	
+		wCItem.animate({'top':'80px','opacity':'1'},800, function(){
+			bChair.animate({'left':'30%'},1200,function(){
+				bCItem.animate({'top':'73px','opacity':'1'},1000, function(){
+					textContainer.animate({'bottom':'350px'},800);
 				});
 			});
 		});
@@ -29,15 +35,15 @@ $(document).ready(function(){
 	var height = $(window).height();
 	if (height < 800){
 		$('p').css({'margin':'0'})
-		$('#Container .textContainer').addClass('lowHeight');
+		textContainer.addClass('lowHeight');
 	}
 
 	$('#Container .letsText').click(function(){
-		$('#Container .bossChair').fadeOut(
+		bChair.fadeOut(
 			function(){
-				$('#Container .workerChair').fadeOut(
+				wChair.fadeOut(
 					function(){
-						$('#Container .textContainer').animate({'bottom':'-500px'},function(){
+						textContainer.animate({'bottom':'-500px'},function(){
 							$(this).fadeOut(
 								function(){
 									$('#SendFormContainer').fadeIn(function(){
@@ -51,5 +57,7 @@ $(document).ready(function(){
 			}
 		);
 	});
+
+
 
 });
